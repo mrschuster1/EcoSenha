e4b2e40b9a06a271b74b1cc72da8ccc936986cde
Thiago Schuster <99575284+mrschuster1@users.noreply.github.com>
2024-01-01 20:39:20 -0300

    Add XML Doc to Controller.Senha

--- src/controller/EcoSenha.Controller.Senha.pas
+++ src/controller/EcoSenha.Controller.Senha.pas
@@ -10,30 +10,77 @@ uses
   IdHash;
 
 type
+  /// <summary>
+  /// Interface para geração e gerenciamento de senhas para um período específico.
+  /// </summary>
   ISenha = interface
     ['{9EF2FB47-5265-414B-BD18-E978E7E9E73F}']
+
+    /// <summary>
+    /// Obtém a senha atual para o período ativo.
+    /// </summary>
     function SenhaAtual: string;
+
+    /// <summary>
+    /// Gera a próxima senha para o próximo período.
+    /// </summary>
     function ProximaSenha: string;
+
+    /// <summary>
+    /// Obtém a data e hora de início do período atual da senha.
+    /// </summary>
     function InicioPeriodoAtual: TDateTime;
+
+    /// <summary>
+    /// Obtém a data e hora de término do período atual da senha.
+    /// </summary>
     function FimPeriodoAtual: TDateTime;
+
+    /// <summary>
+    /// Obtém a data e hora de início do próximo período da senha.
+    /// </summary>
     function InicioProximoPeriodo: TDateTime;
+
+    /// <summary>
+    /// Obtém a data e hora de término do próximo período da senha.
+    /// </summary>
     function FimProximoPeriodo: TDateTime;
   end;
 
 type
+  /// <summary>
+  /// Implementação da interface ISenha para geração de senhas.
+  /// </summary>
   TSenha = class(TInterfacedObject, ISenha)
   private
+    /// <summary>
+    /// Calcula o hash MD5 do valor de string fornecido.
+    /// </summary>
     class function GetMD5(const AValue: string): string;
 
   public
+    /// <summary>
+    /// Cria uma nova instância de ISenha.
+    /// </summary>
     class function New: ISenha;
-    function SenhaAtual: string;
-    function ProximaSenha: string;
-    function InicioPeriodoAtual: TDateTime;
-    function FimPeriodoAtual: TDateTime;
-    function InicioProximoPeriodo: TDateTime;
-    function FimProximoPeriodo: TDateTime;
 
+    /// <inheritdoc/>
+    function SenhaAtual: string;
+
+    /// <inheritdoc/>
+    function ProximaSenha: string;
+
+    /// <inheritdoc/>
+    function InicioPeriodoAtual: TDateTime;
+
+    /// <inheritdoc/>
+    function FimPeriodoAtual: TDateTime;
+
+    /// <inheritdoc/>
+    function InicioProximoPeriodo: TDateTime;
+
+    /// <inheritdoc/>
+    function FimProximoPeriodo: TDateTime;
   end;
 
 implementation
@@ -45,7 +92,7 @@ begin
   if DayOfTheMonth(Now) <= 15 then
     Result := (IncDay(StartOfTheMonth(Now), 15)) - OneHour
   else
-    Result := EndOfTheMonth(Now)
+    Result := EndOfTheMonth(Now);
 end;
 
 function TSenha.FimProximoPeriodo: TDateTime;
@@ -85,19 +132,19 @@ end;
 
 class function TSenha.New: ISenha;
 begin
-  Result := Self.Create
+  Result := Self.Create;
 end;
 
 function TSenha.ProximaSenha: string;
 begin
-  Result := 'ECO' + Copy(GetMD5('supervisor_' +
-    FormatDateTime('yyyyMMdd', InicioProximoPeriodo)), 1, 3);
+  Result := 'ECO' + Copy(GetMD5('supervisor_' + FormatDateTime('yyyyMMdd',
+    InicioProximoPeriodo)), 1, 3);
 end;
 
 function TSenha.SenhaAtual: string;
 begin
-  Result := 'ECO' + Copy(GetMD5('supervisor_' +
-    FormatDateTime('yyyyMMdd', Now)), 1, 3);
+  Result := 'ECO' + Copy(GetMD5('supervisor_' + FormatDateTime('yyyyMMdd',
+    Now)), 1, 3);
 end;
 
 end.
